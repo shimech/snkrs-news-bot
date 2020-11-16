21c54bc310fab8ad1da508c6361102c0dacfe29a
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-11-16 21:52:32 +0900

    complete

--- src/crawler.py
+++ src/crawler.py
@@ -1,7 +1,12 @@
 import os
 import time
 from dotenv import load_dotenv
+import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 load_dotenv()
 
 
@@ -9,21 +14,69 @@ class Crawler:
     TIMELINE_URL = os.environ["URL"]
     STOCK_URL = TIMELINE_URL + "?s=in-stock"
     UPCOMING_URL = TIMELINE_URL + "?s=upcoming"
+    DATABASE_DIRPATH = os.path.join(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__))), "database")
     CHROMEDRIVER_PATH = os.path.join(os.path.dirname(os.path.dirname(
         os.path.abspath(__file__))), "chromedriver")
+    TIMEOUT = 10
 
     @classmethod
     def run(cls, mode="timeline"):
-        driver = cls.__init_webdriver()
-        time.sleep(3)
-        driver.quit()
+        df = cls.__get_database(mode)
 
-        target_url = cls.TIMELINE_URL if mode == "timeline" else cls.STOCK_URL
-        return [{
-            "name": "name",
-            "url": target_url,
-            "is_snkrs_pass": False
-        }]
+        driver = cls.__init_webdriver()
+        wait = WebDriverWait(driver, cls.TIMEOUT)
+
+        try:
+            init_url = cls.UPCOMING_URL
+            driver.get(init_url)
+            wait.until(EC.element_to_be_clickable(
+                (By.CLASS_NAME, "nav-items")))
+
+            nav_items = driver.find_elements_by_class_name("nav-items")
+            for nav_item in nav_items:
+                if nav_item.text == cls.__select_target_tab(mode):
+                    nav_item.click()
+                    wait.until(EC.element_to_be_clickable(
+                        (By.CLASS_NAME, "load-more")))
+                    break
+
+            card_links = driver.find_elements_by_class_name("card-link")
+            card_dicts = []
+            for card_link in card_links:
+                card_dicts.append({
+                    "name": card_link.get_attribute("aria-label"),
+                    "url": card_link.get_attribute("href")
+                })
+
+            driver.quit()
+
+            news_list = []
+            for card_dict in card_dicts:
+                if not card_dict.get("url") in df["url"].values.tolist():
+                    news = card_dict
+                    news["is_snkrs_pass"] = "pass" in card_dict["url"] and mode == "timeline"
+                    news_list.append(news)
+                    df = df.append(news, ignore_index=True)
+                    print("New Post: {}".format(news))
+            df.to_csv(cls.__select_database_path(mode), index=False)
+
+            return news_list
+
+        except TimeoutException as e:
+            print("TIMEOUT")
+            driver.quit()
+            return
+
+    @classmethod
+    def __select_database_path(cls, mode):
+        csvname = "{}.csv".format(mode)
+        return os.path.join(cls.DATABASE_DIRPATH, csvname)
+
+    @classmethod
+    def __get_database(cls, mode):
+        df = pd.read_csv(cls.__select_database_path(mode))
+        return df
 
     @classmethod
     def __init_webdriver(cls):
@@ -36,3 +89,21 @@ class Crawler:
             options=options
         )
         return driver
+
+    @staticmethod
+    def __select_target_tab(mode):
+        if mode == "timeline":
+            return "タイムライン"
+        elif mode == "stock":
+            return "在庫あり"
+        else:
+            return "タイムライン"
+
+    @classmethod
+    def __select_target_url(cls, mode):
+        if mode == "timeline":
+            return cls.TIMELINE_URL
+        elif mode == "stock":
+            return cls.STOCK_URL
+        else:
+            return cls.TIMELINE_URL
--- src/main.py
+++ src/main.py
@@ -13,15 +13,15 @@ def main():
     mode = "stock" if argument_parser.arguments.stock else "timeline"
     news_list = Crawler.run(mode=mode)
 
-    # if news_list is not None:
-    #     slack_bot = SlackBot(
-    #         os.environ["CHANNEL"], os.environ["TEST_CHANNEL"], os.environ["SLACK_API_TOKEN"])
-    #     for news in news_list:
-    #         message = Message.make_message(news, mode)
-    #         slack_bot.post_message(
-    #             message,
-    #             is_test=argument_parser.arguments.test
-    #         )
+    if len(news_list) > 0:
+        slack_bot = SlackBot(
+            os.environ["CHANNEL"], os.environ["TEST_CHANNEL"], os.environ["SLACK_API_TOKEN"])
+        for news in news_list:
+            message = Message.make_message(news, mode)
+            slack_bot.post_message(
+                message,
+                is_test=argument_parser.arguments.test
+            )
 
 
 if __name__ == "__main__":
